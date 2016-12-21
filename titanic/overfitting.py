ecfb8eabb3f97a6c0a46fa2eec62403bc7c9c064
shiying1 <shiying1@ZB-PC0DLKP0.360buyAD.local>
2016-12-21 13:24:54 +0800

    bug in overfitting fixed

--- titanic/overfitting.py
+++ titanic/overfitting.py
@@ -16,9 +16,9 @@ train = u.prepare_data(train)
 feature_list = ["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "PassengerId"]
 
 #Control overfitting by setting "max_depth" to 10 and "min_samples_split" to 5 : my_tree_two
-max_depth = 5
-min_samples_split = 1000
-my_tree, features, target = u.train_model(train, feature_list)
+max_depth = 10
+min_samples_split = 5
+my_tree, features, target = u.train_model(train, feature_list, max_depth, min_samples_split)
 
 
 #Print the score of the new decison tree
